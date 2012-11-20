e04c0757044935237b0507a2722e5265e677b542
Edison Trent <edison.trent@freeport7.org>
2012-11-21 00:42:15 +0400

    Fixed autobackup script

--- auto-backup.rb
+++ auto-backup.rb
@@ -1,8 +1,12 @@
 date = Time.now
 date = "-#{date.month}-#{date.year}"
-system("rm -r /srv/tachyon/backups/files/#{(Time.now - (3*86400)).day}#{date}")
-system("rm /srv/tachyon/backups/database/#{(Time.now - (3*86400)).day}#{date}.mysql2")
-system("cp -r /srv/tachyon/engine/public/files /srv/tachyon/backups/files/#{Time.now.day}#{today}")
+begin
+  system("rm -r /srv/tachyon/backups/files/#{(Time.now - (3*86400)).day}#{date}")
+  system("rm /srv/tachyon/backups/database/#{(Time.now - (3*86400)).day}#{date}.mysql2")
+rescue 
+  # fuck it
+end
+system("cp -r /srv/tachyon/engine/public/files /srv/tachyon/backups/files/#{Time.now.day}#{date}")
 system("thin -C /srv/tachyon/engine/config/thin.yml stop -T0")
 system("mysqldump -utrent -prosenkristall freeport7 > /srv/tachyon/backups/database/#{Timw.now.day}#{date}")
 system("thin -C /srv/tachyon/engine/config/thin.yml start")
