afdb5b8460d7f44d2ba18e521edb7c0ed9beff94
Edison Trent <edison.trent@freeport7.org>
2012-11-21 03:15:57 +0400

    Changed version naming convention

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Tachyon
     config.encoding = "utf-8"
     config.filter_parameters += ["message[password]", "password"]
     config.assets.enabled = true
-    config.assets.version = "1.06"
+    config.assets.version = "1.06.312"
   end
 end
