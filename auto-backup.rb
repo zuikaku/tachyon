e58d363895279ec5bfbd368603ddfc165f2b9ca3
Edison Trent <edison.trent@freeport7.org>
2012-11-21 00:40:24 +0400

    Added auto-backup script

--- auto-backup.rb
+++ auto-backup.rb
@@ -1,4 +1,8 @@
 date = Time.now
-today = "#{date.day}-#{date.month}-#{date.year}"
-four_days_ago = "#{date.day-4}-#{date.month}-#{date.year}"
-
+date = "-#{date.month}-#{date.year}"
+system("rm -r /srv/tachyon/backups/files/#{(Time.now - (3*86400)).day}#{date}")
+system("rm /srv/tachyon/backups/database/#{(Time.now - (3*86400)).day}#{date}.mysql2")
+system("cp -r /srv/tachyon/engine/public/files /srv/tachyon/backups/files/#{Time.now.day}#{today}")
+system("thin -C /srv/tachyon/engine/config/thin.yml stop -T0")
+system("mysqldump -utrent -prosenkristall freeport7 > /srv/tachyon/backups/database/#{Timw.now.day}#{date}")
+system("thin -C /srv/tachyon/engine/config/thin.yml start")
